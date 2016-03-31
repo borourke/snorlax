1cc85229ebe3c3634d8008217dcfdff9c0089d34
Benjamin Beckerman <benjamin.beckerman@crowdflower.com>
2016-03-31 11:41:27 -0700

    updated projects json payload

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,9 @@ class Project < ActiveRecord::Base
 
   def self.format_json_for_index
     self.all.order(created_at: :desc).each_with_object({}) do |project, index_hash|
-      index_hash[project.name] = project.jobs.each_with_object({}) do |job, jobs_hash|
+      project_hash = index_hash[project.name] = {}
+      project_hash[:id] = project.id
+      project_hash[:jobs] = project.jobs.each_with_object({}) do |job, jobs_hash|
         jobs_hash[job.alias.to_sym] = job.percent_complete
       end
     end
